de289f74a5b25907b7532b7b832954134f4ec92d
Alan Bouzali <abouzali@yahoo.com>
2024-04-15 19:40:27 -0300

    sube la cant comprada al carrito

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,8 +1,16 @@
 //import ClassItem from '../Item/Item.module.css';
+import { useState } from 'react';
 import ClassItem from '../ItemDetail/Detail.module.css';
 import ItemCount from '../ItemCount/ItemCount.jsx';
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
+  const [quantityAdded, setQuantityAdded] = useState(0);
+
+  const handleOnAdd = (quantity) => {
+    setQuantityAdded(quantity);
+  };
+
   return (
     <article className={ClassItem.CardItem}>
       <header className="Header">
@@ -17,13 +25,20 @@ const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
         <p className="Info">Descripción: {description}</p>
       </section>
       <footer className="ItemFooter">
-        <p className="Info">Stock disponible: {stock}</p>
+        {quantityAdded > 0 ? (
+          <Link to="/cart" className={ClassItem.ButtonDetail}>
+            Terminar compra
+          </Link>
+        ) : (
+          <ItemCount id={id} initial={1} stock={stock} onAdd={handleOnAdd} />
+        )}
+        {/*   <p className="Info">Stock disponible: {stock}</p>
         <ItemCount
           id={id}
           initial={1}
           stock={stock}
           onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}
-        />
+        /> */}
       </footer>
     </article>
   );
